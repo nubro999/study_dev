c62249e00f92b5647f9c90f8a857ad54c20bb528
nubro999 <zcb167@gmail.com>
2024-10-14 17:22:15 +0900

    OrderService

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -2,11 +2,15 @@ package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -22,4 +26,23 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
+    //생성메서드//
+    public static OrderItem creatOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+//비즈니스로직//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
