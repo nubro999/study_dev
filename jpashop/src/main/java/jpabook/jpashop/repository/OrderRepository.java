654f21cb69d8b8dadc19d936fceed2e85cef0f5b
nubro999 <zcb167@gmail.com>
2024-11-08 18:17:29 +0900

    OrderSearch

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -20,5 +20,5 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> findAll(OrderSearch orderSearch) {}
+    //public List<Order> findAll(OrderSearch orderSearch) {
 }
